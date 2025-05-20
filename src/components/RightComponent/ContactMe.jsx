6f8e31835f2b3d136381a6798d340a6f3866f794
nikitasethia16 <nikitasethia1603@gmail.com>
2025-05-20 12:59:41 +0530

    update

--- src/components/RightComponent/ContactMe.jsx
+++ src/components/RightComponent/ContactMe.jsx
@@ -7,66 +7,73 @@ const formFields = [
   { name: "phone", label: "Phone Number", type: "tel", required: true },
   { name: "company", label: "Your Company", type: "text", required: false },
   { name: "subject", label: "Subject", type: "text", required: true },
-  { name: "question", label: "Your Question", type: "textarea", required: true },
+  {
+    name: "question",
+    label: "Your Question",
+    type: "textarea",
+    required: true,
+  },
 ];
 
 const ContactMe = () => {
- 
- return (
+  return (
     <>
-    <h2 className="mt-24 text-lg font-medium tracking-tight text-[rgb(152,243,151)] sm:text-xl text-center mb-4">
-       Let's Connect
+      <h2 className="mt-24 text-lg font-medium tracking-tight text-[rgb(152,243,151)] sm:text-xl text-center mb-4">
+        Let's Connect
       </h2>
       <p className="text-sm text-white text-center mb-10">
-        Get in touch with me to provide better service. Social media success starts with a conversation let’s start.
+        Get in touch with me to provide better service. Social media success
+        starts with a conversation let’s start.
       </p>
-    <form
-      className="max-w-xl mx-auto p-6 bg-gray-900 text-white rounded-lg shadow-md space-y-4"
-      action="https://formsubmit.co/7c191778ad16041f91d5656a4f9ca43c"
-      method="POST"
-    >
-
-      {formFields.map(({ name, label, type, required }) => (
-        <div key={name}>
-          <label className="block mb-1 font-semibold" htmlFor={name}>
-            {label}
-          </label>
-
-          {type === "textarea" ? (
-            <textarea
-              id={name}
-              name={name}
-              rows="4"
-              required={required}
-              className="w-full p-2 rounded bg-gray-800 text-white"
-            />
-          ) : (
-            <input
-              id={name}
-              name={name}
-              type={type}
-              required={required}
-              className="w-full p-2 rounded bg-gray-800 text-white"
-            />
-          )}
-        </div>
-      ))}
-      <input type="hidden" name="_captcha" value="false"></input>
-       <input type="hidden" name="_next" value="https://vishakha07-portfolio.netlify.app/thank-you"></input>
-
-      <button
-        type="submit"
-        className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded font-semibold"
+      <form
+        className="max-w-xl mx-auto p-6 bg-gray-900 text-white rounded-lg shadow-md space-y-4"
+        action="https://formsubmit.co/7c191778ad16041f91d5656a4f9ca43c"
+        method="POST"
       >
-        Submit
-      </button>
-    </form>
-     {/* Social Icons */}
-      <div className="flex gap-4 mt-10 lg:hidden justify-center"> 
-        <SocialIcons/>
+        {formFields.map(({ name, label, type, required }) => (
+          <div key={name}>
+            <label className="block mb-1 font-semibold" htmlFor={name}>
+              {label}
+            </label>
+
+            {type === "textarea" ? (
+              <textarea
+                id={name}
+                name={name}
+                rows="4"
+                required={required}
+                className="w-full p-2 rounded bg-gray-800 text-white"
+              />
+            ) : (
+              <input
+                id={name}
+                name={name}
+                type={type}
+                required={required}
+                className="w-full p-2 rounded bg-gray-800 text-white"
+              />
+            )}
+          </div>
+        ))}
+        <input type="hidden" name="_captcha" value="false"></input>
+        <input
+          type="hidden"
+          name="_next"
+          value={`${window.location.origin}/thank-you`}
+        />
+
+        <button
+          type="submit"
+          className="w-full py-2 bg-blue-600 hover:bg-blue-700 rounded font-semibold"
+        >
+          Submit
+        </button>
+      </form>
+      {/* Social Icons */}
+      <div className="flex gap-4 mt-10 lg:hidden justify-center">
+        <SocialIcons />
       </div>
     </>
-
   );
 };
 
